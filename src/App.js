ff1ef3ecc190b25e17ea324e20b57aeb9d8e4633
x <thisisthetrue@hotmail.com>
2022-08-08 20:01:21 -0500

    sin formularios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Soycliente from './pages/Soycliente'
+import Clienteregister from './pages/Clienteregister'
+import Soyexperto from './pages/Soyexperto'
+import Expertoregister from './pages/Expertoregister'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route index element={<Home />} />
+    <Route path="soycliente" element={<Soycliente />} />
+    <Route path="clienteregister" element={<Clienteregister />} />
+    <Route path="soyexperto" element={<Soyexperto />} />
+    <Route path="expertoregister" element={<Expertoregister />} />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
