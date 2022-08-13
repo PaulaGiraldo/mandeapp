2b17480a42d08a300aa19e2e00d7ba9cdc751bb6
Juan Arias <62755247+bluruwu@users.noreply.github.com>
2022-08-12 23:40:06 -0500

    Delete src directory

--- src/contentRec.jsx
+++
@@ -1,9 +0,0 @@
-export default function ContentRec(props){
-  return(
-    <div id="inicioRec" style={{
-        backgroundColor: props.bgcolor,
-      }}>
-    {props.title} <br/> {props.contenido}
-    </div>
-  )
-}
